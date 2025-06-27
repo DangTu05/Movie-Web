85155d95d04d2a2b03dd07d467763fd55687dfa8
Dang Tu <dtu161205@gmail.com>
2025-06-27 11:48:09 +0700

    Call Api lấy ra các tỉnh và render ra giao diện setting

--- server/src/controllers/admin/SettingController.ts
+++ server/src/controllers/admin/SettingController.ts
@@ -1,7 +1,11 @@
 import { Request, Response, NextFunction } from "express";
+import getProvinces from "../../helpers/api/getProvinces";
 class SettingController {
   public async render(req: Request, res: Response): Promise<void> {
-    res.render("admin/pages/setting");
+    const provinces = await getProvinces();
+    res.render("admin/pages/setting", {
+      provinces: provinces
+    });
   }
 }
 export default SettingController;
