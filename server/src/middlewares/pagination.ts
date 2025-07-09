7304109aead329b55b3411015c92d59bbf1c7048
Dang Tu <dtu161205@gmail.com>
2025-07-09 20:25:40 +0700

    Tạo view show danh sách voucher

--- server/src/middlewares/pagination.ts
+++ server/src/middlewares/pagination.ts
@@ -2,9 +2,11 @@ import { NextFunction, Request, Response } from "express";
 import { IPagination } from "../interfaces/IPagination";
 export const paginationMiddleware = (req: Request, res: Response, next: NextFunction) => {
   const defaultPagination: IPagination = {
-    currentPage: parseInt(String(req.query.page || "1")),
-    limit: parseInt(String(req.query.limit || "10")),
-    skip: (parseInt(String(req.query.page || "1")) - 1) * parseInt(String(req.query.limit || "10")),
+    currentPage: parseInt(String(req.query.page || "1")) === 0 ? 1 : parseInt(String(req.query.page || "1")),
+    limit: parseInt(String(req.query.limit || "10")) === 0 ? 10 : parseInt(String(req.query.limit || "10")),
+    skip:
+      (parseInt(String(req.query.page === "0" ? "1" : req.query.page || "1")) - 1) *
+      parseInt(String(req.query.limit === "0" ? "10" : req.query.limit || "10")),
     totalPage: 0,
     count: 0
   };
--- server/src/services/admin/VoucherService.ts
+++ server/src/services/admin/VoucherService.ts
@@ -5,6 +5,8 @@ import { existVoucher } from "../../helpers/existVoucher";
 import mongoose from "mongoose";
 import logger from "../../configs/logger";
 import Constants from "../../utils/Constant";
+import { IPagination } from "../../interfaces/IPagination";
+import { formatDate } from "../../utils/formatDate";
 class VoucherService extends BaseService<IVoucher, IVoucherInput> {
   protected model = voucherModel;
   public getVoucherStatus(startDate: Date, endDate: Date) {
@@ -24,6 +26,41 @@ class VoucherService extends BaseService<IVoucher, IVoucherInput> {
       return;
     }
     return await this.model.findOne({ _id: id, deleted: false }).select(Constants.COMMON_SELECT_FIELDS).lean();
+  }
+  public async getAllVoucher(pagination?: IPagination) {
+    if (!pagination) {
+      const vouchers: IVoucher[] = await voucherModel
+        .find({ deleted: false })
+        .select(Constants.COMMON_SELECT_FIELDS)
+        .lean();
+      if (vouchers.length === 0) {
+        logger.info("No vouchers found");
+      }
+      return vouchers;
+    } else {
+      const [vouchers, count] = await Promise.all([
+        voucherModel
+          .find({ deleted: false })
+          .select(Constants.COMMON_SELECT_FIELDS)
+          .skip(pagination.skip)
+          .limit(pagination.limit)
+          .lean(),
+        voucherModel.countDocuments({ deleted: false })
+      ]);
+      pagination.count = count;
+      pagination.totalPage = Math.ceil(count / pagination.limit);
+      const formatedVouchers = vouchers.map((voucher) => ({
+        ...voucher,
+        voucher_start: formatDate(voucher.voucher_start),
+        voucher_end: formatDate(voucher.voucher_end)
+      }));
+      return {
+        pagination: {
+          ...pagination
+        },
+        vouchers: formatedVouchers
+      };
+    }
   }
 }
 export default VoucherService;
