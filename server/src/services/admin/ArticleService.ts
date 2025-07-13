e041c9d44b2c220223d59e4fbfb29256d6a3c3b8
Dang Tu <dtu161205@gmail.com>
2025-07-14 00:06:03 +0700

    Tạo view danh sách bài viết

--- server/src/services/admin/ArticleService.ts
+++ server/src/services/admin/ArticleService.ts
@@ -4,10 +4,41 @@ import Constants from "../../utils/Constant";
 import BaseService from "./BaseService";
 import articleModel, { IArticle } from "../../models/schema/articleSchema";
 import { IArticleInput } from "../../interfaces/IArticleInput";
+import { IPagination } from "../../interfaces/IPagination";
 class ArticleService extends BaseService<IArticle, IArticleInput> {
   protected model = articleModel;
   protected async checkId(id: string): Promise<void> {
     return;
+  }
+  public async getAllArticle(pagination?: IPagination) {
+    if (!pagination) {
+      const articles: IArticle[] = await this.model
+        .find({ deleted: false })
+        .select(Constants.COMMON_SELECT_FIELDS)
+        .lean();
+      if (articles.length === 0) {
+        logger.info("No articles found");
+      }
+      return articles;
+    } else {
+      const [articles, count] = await Promise.all([
+        this.model
+          .find({ deleted: false })
+          .select(Constants.COMMON_SELECT_FIELDS)
+          .skip(pagination.skip)
+          .limit(pagination.limit)
+          .lean(),
+        this.model.countDocuments({ deleted: false })
+      ]);
+      pagination.count = count;
+      pagination.totalPage = Math.ceil(count / pagination.limit);
+      return {
+        pagination: {
+          ...pagination
+        },
+        articles
+      };
+    }
   }
   public async findArticleById(id: string) {
     if (!mongoose.Types.ObjectId.isValid(id)) {
