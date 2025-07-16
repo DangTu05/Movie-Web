0c892a93978a3c98d370abcd2f8d886f39cd412c
Dang Tu <dtu161205@gmail.com>
2025-07-16 23:48:10 +0700

    Render tin tức ra giao diện home

--- server/src/controllers/client/HomeController.ts
+++ server/src/controllers/client/HomeController.ts
@@ -1,15 +1,26 @@
 import { Request, Response, NextFunction } from "express";
 import errorHandler from "../../utils/handler/handleAsync";
 import { getComingSoonMovies, getNowPlayingMovies } from "../../services/common/movie";
+import ArticleService from "../../services/admin/ArticleService";
+const _articleService = new ArticleService();
 class HomeController {
   constructor() {
     this.showView = errorHandler.handleAsyncErrors(this.showView.bind(this));
   }
   public async showView(req: Request, res: Response, next: NextFunction) {
     const data: any = {};
-    const [nowPlaying, comingSoon] = await Promise.all([getNowPlayingMovies(), getComingSoonMovies()]);
+    req.pagination.limit = 4;
+    const [nowPlaying, comingSoon, articles] = await Promise.all([
+      getNowPlayingMovies(),
+      getComingSoonMovies(),
+      _articleService.getAllArticle(req.pagination)
+    ]);
     data.comingSoonMovies = comingSoon;
     data.nowPlayingMovies = nowPlaying;
+    if (!Array.isArray(articles)) {
+      data.articles = articles.articles;
+      // data.pagination = articles.pagination;
+    }
     data.movies = [...nowPlaying, ...comingSoon];
     res.render("client/pages/home", {
       data: data
